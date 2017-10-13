5a0c82c64240e0f43aa1d53272fbbada9b3435cf
pradeep <pradeep@example.com>
2017-10-13 20:36:24 +0000

    [*] Using bn.js now. [*] Calculating y properly now - using quadric residue modulo p.

--- index.js
+++ index.js
@@ -13,33 +13,65 @@ function main(maxAttempts) {
     y: new BN(2)
   };
   var curveOrder = new BN("21888242871839275222246405745257275088548364400416034343698204186575808495617");
+      curveOrder = new BNJS(curveOrder.toString(16), 16);
+
+  var red = BNJS.red(curveOrder);
+  var curveOrderRed = curveOrder.toRed(red);
+  //var fourRed =
+
 
   var sha3_256  = function(message) { return CryptoJS.SHA3(message, { outputLength: 256 }) };
   var pointWordArray      = CryptoJS.enc.Hex.parse(point2hex(point));
   var pointHashWordArray  = sha3_256(pointWordArray); // keccak256
   var pointHashScalar     = new BN( '0x' + CryptoJS.enc.Hex.stringify(pointHashWordArray) );
   pointHashScalar = pointHashScalar.mod(curveOrder);
+  var lagrange = curveOrder.add(new BNJS(1)).div(new BNJS(4));
+  // console.log("curveOrder=", curveOrder.toString(10));
+  // console.log("lagrange=", lagrange.toString(10));
+  // console.log("lagrange.mod(curveOrder)=", lagrange.mod(curveOrder).toString(10));
+  // console.log("lagrange.mod(curveOrder).mul(4)=", lagrange.mod(curveOrder).mul(new BNJS(4)).toString(10));
 
   var x, y, found = false;
   for(var i=0; i < maxAttempts; ++i){
     x   = pointHashScalar.add(i).mod(curveOrder);
-    var y2  = x.pow(3, curveOrder).add(3).mod(curveOrder);
+    var xRed = (new BNJS(x.toString(16), 16)).toRed(red);
+    var y2    = x.pow(3, curveOrder).add(3).mod(curveOrder);
+    var y2Red = xRed.redPow(new BNJS(3)).redAdd( (new BNJS(3)).toRed(red) );
+
     // y   = y2.sqrt(); // this is incorrect way of finding square root
-    x           = new BNJS(x.toString(16), 16);
-    y2           = new BNJS(y2.toString(16), 16);
-    curveOrder  = new BNJS(curveOrder.toString(16), 16);
-    y = x.pow(
-      curveOrder.add(new BNJS(1)).div(new BNJS(4))
-    ).mod(curveOrder);
+    y2          = new BNJS(y2.toString(16), 16);
+    // following does not work because curve order is not n ≡ 3 (mod 4)
+    var yRedLagrange = y2Red.redPow(
+      lagrange
+    );
+
+    try {
+      y = y2Red.redSqrt().fromRed();
+    } catch(e) {
+      // non-residue
+      continue;
+    }
+    //.mod(curveOrder);
     // x^((field modulus + 1)/4)
-    if (y.pow(2, curveOrder).eq(y2)) {
+    if (y.pow(new BNJS(2)).mod(curveOrder).eq(y2)) {
       found = true;
       console.log('pointHash found in ' + (i+1) + ' attempts.');
+      console.log("x=", x.toString(10));
+      console.log("y=", y.toString(10));
       break;
     } else {
       if (i < 100) {
+        console.log('---');
+        console.log("x=", x.toString(10));
         console.log("y2=", y2.toString(10));
-        console.log("y.pow(2)=", y.pow(2, curveOrder).toString(10));
+        console.log("y2Red=", y2Red.fromRed().toString(10));
+        console.log("y=", y.toString(10));
+        // console.log("y.pow(2).mod(curveOrder)=", y.pow(new BNJS(2)).mod(curveOrder).toString(10));
+        // console.log("yRed.redPow(2)=", yRed.redPow(new BNJS(2)).fromRed().toString(10));
+        // console.log('y2Red.redSqrt()=', y2Red.redSqrt().fromRed().toString(10));
+        // console.log('y2Red.redSqrt().redPow(2)=', y2Red.redSqrt().redPow(new BNJS(2)).fromRed().toString(10));
+        console.log('---');
+
       }
     }
   }
@@ -55,4 +87,4 @@ function point2hex(point){
   return pointHex_x + pointHex_y;
 }
 
-main(1000);
+main(10);
